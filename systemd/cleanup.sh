efee5488f067cbf1726bb5a1bec574e8e05aa154
bruno <bruno@ancalagon.be>
2025-01-26 14:30:01 +0100

    Added a cleanup.sh script to clean all configuration

--- systemd/cleanup.sh
+++ systemd/cleanup.sh
@@ -43,11 +43,11 @@ else
 fi
 
 # Execute commands
-execute_command "Stopping $NAME pod..." "systemctl $SUSER stop ${NAME}-pod"
-execute_command "Stopping  $NAME network..." "systemctl $SUSER stop ${NAME}-network"
+systemctl $SUSER stop "${NAME}"-pod
+systemctl $SUSER stop "${NAME}"-network
 
-execute_command "Removing $NAME pod..." "podman pod rm ${NAME}"
-execute_command "Removing $NAME network..." "podman network rm -f ${NAME}"
+podman pod rm "${NAME}"
+podman network rm -f "${NAME}"
 
 echo Removing all quadlet file from systemd
 rm -rf "${DESTINATION}/$NAME-*"
