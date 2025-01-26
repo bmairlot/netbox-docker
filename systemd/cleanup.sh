063d255c39d854222252fb9bc5c6d31d5cb31edb
bruno <bruno@ancalagon.be>
2025-01-26 14:13:44 +0100

    Added a cleanup.sh script to clean all configuration

---
+++ systemd/cleanup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Function to display error messages
+error_exit() {
+    echo "Error: $1" >&2
+    exit 1
+}
+
+# Function to execute command and handle errors
+execute_command() {
+   local description="$1"
+   local command="$2"
+
+   echo "$description"
+   if ! $command; then
+       echo "Error: Command '$command' failed with exit code $?"
+       exit 1
+   fi
+}
+
+# Default name
+NAME="netbox"
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --name) NAME="$2"; shift ;;
+        *) error_exit "Unknown parameter: $1" ;;
+    esac
+    shift
+done
+
+SUSER=""
+# Set DESTINATION based on whether the user is root or not
+if [ "$(id -u)" -eq 0 ]; then
+    DESTINATION="/etc/containers/systemd"
+else
+    DESTINATION="${HOME}/.config/containers/systemd"
+    SUSER="--user"
+fi
+
+# Execute commands
+execute_command "Stoping $NAME pod..." "systemctl $SUSER stop ${NAME}-pod"
+execute_command "Starting $NAME network..." "systemctl $SUSER start ${NAME}-network"
+
+echo Removing all quadlet file from systemd
+rm -rf "${DESTINATION}/$NAME-*"
+
+
+# Execute commands
+execute_command "Reloading quadlet files" "systemctl $SUSER daemon-reload"
+execute_command "Starting $NAME network..." "systemctl $SUSER start ${NAME}-network"
+execute_command "Starting $NAME pod..." "systemctl $SUSER start ${NAME}-pod"
